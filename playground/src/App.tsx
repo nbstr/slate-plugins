bc482305238de1682a9a2d04f255550c55641f14
nab <nab@get-hey.com>
2021-05-03 05:08:25 +0200

    Revert "up"
    This reverts commit 8a217cda5a9e4df42fd6db820423b5cb6902b516.

---
+++ playground/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Playground from "./Playground";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Playground.</h1>
+      <Playground />
+    </div>
+  );
+}
+
+export default App;
